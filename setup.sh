02ad2d2def9fd9bf50a50b9d6ef24fe81542472b
Vit <103600474+Vitlyr@users.noreply.github.com>
2023-10-18 16:58:18 -0400

    Installation Wizard Fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# !! WARNING: DO NOT EDIT IN ANY WAY
+# !! DO NOT EDIT THIS INSTALLATION SCRIPT
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pass=$1
 cloudflare_token=$2
@@ -22,10 +24,11 @@ if [[ "$cld_agreement" != "y" ]]; then
   echo "Please look over Cloudflare's Terms and agree."
   exit 1
 fi
+
 echo "NOTICE: \n No information of yours will be shared \n DO NOT edit any of the files (except for config.sh). (y/n)"
 read -r rules_agreement
 if [[ "$rules_agreement" != "y" ]]; then
-  echo "This is a requirement, you have to accept to show your understandment."
+  echo "This is a requirement, you have to accept to show your understanding."
   exit 1
 fi
 
@@ -33,11 +36,12 @@ echo $pass | sudo -s apt install pv -y
 echo $pass | sudo -s apt install gnome-terminal -y | pv -p -t -e -r -a -b > /dev/null
 echo $pass | sudo -s apt update -y | pv -p -t -e -r -a -b > /dev/null
 
-echo $pass | sudo -s chmod +x run.sh
-echo $pass | sudo -s chmod +x config.sh
-echo $pass | sudo -s chmod +x uninstall.sh
+# Change permissions of run.sh, config.sh, and uninstall.sh in the script's directory
+echo $pass | sudo -s chmod +x "$SCRIPT_DIR/run.sh"
+echo $pass | sudo -s chmod +x "$SCRIPT_DIR/config.sh"
+echo $pass | sudo -s chmod +x "$SCRIPT_DIR/uninstall.sh"
 
-echo "pass=$pass" > config.sh
-echo "cloudflare_token=$cloudflare_token" > config.sh
+echo "pass=$pass" > "$SCRIPT_DIR/config.sh"
+echo "cloudflare_token=$cloudflare_token" >> "$SCRIPT_DIR/config.sh"
 
 echo "Setup Complete!"
