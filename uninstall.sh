dd84bdf61d4e946cf1629c3a4e0cef46b76c4d33
Vit <103600474+Vitlyr@users.noreply.github.com>
2023-10-18 16:14:48 -0400

    Fixed Uninstallation 🛑

--- uninstall.sh
+++ uninstall.sh
@@ -2,12 +2,13 @@
 
 # !! WARNING: DO NOT EDIT IN ANY WAY
 
+source config.sh
+eval "$pass"
 echo "Are you sure you would like to uninstall this program? (y/n)"
 read -r un_agreement
 if [[ "$un_agreement" != "y" ]]; then
   exit 1
 fi
-
 current_dir=$(pwd)
-rm -rf "$current_dir" | pv -p -t -e -r -a -b > /dev/null
+echo $pass | sudo -s rm -rf "$current_dir" -y | pv -p -t -e -r -a -b > /dev/null
 echo "Deleted successfully."
