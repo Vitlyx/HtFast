4e940c6530ff1a6e745e2a6418a60bad450db91d
Vit <103600474+Vitlyr@users.noreply.github.com>
2023-10-18 19:39:12 -0400

    Setup Wizard Update 👾

--- setup.sh
+++ setup.sh
@@ -18,17 +18,11 @@ if [[ $? -ne 0 ]]; then
   exit 1
 fi
 
+echo "NOTICE: No information of yours will be shared, DO NOT edit any of the files (except for ones prompted to do so.)"
 echo "Do you agree to the Cloudflare Terms of Service? (y/n)"
 read -r cld_agreement
 if [[ "$cld_agreement" != "y" ]]; then
   echo "Please look over Cloudflare's Terms and agree."
-  exit 1
-fi
-
-echo "NOTICE: \n No information of yours will be shared \n DO NOT edit any of the files (except for config.sh). (y/n)"
-read -r rules_agreement
-if [[ "$rules_agreement" != "y" ]]; then
-  echo "This is a requirement, you have to accept to show your understanding."
   exit 1
 fi
 
