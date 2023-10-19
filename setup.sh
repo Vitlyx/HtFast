40506d5d80e651e831fb8dd42b8d0e8e0554da08
Vit <103600474+Vitlyr@users.noreply.github.com>
2023-10-19 15:23:26 -0400

    `Setup` Wizard | 🌟

--- setup.sh
+++ setup.sh
@@ -29,9 +29,9 @@ if [[ "$cld_agreement" != "y" ]]; then
 fi
 
 echo $pass | sudo -S apt install pv -y
+echo $pass | sudo -S apt install python3 -y | pv -p -t -e -r -a -b > /dev/null 
 echo $pass | sudo -S apt install gnome-terminal -y | pv -p -t -e -r -a -b > /dev/null
 echo $pass | sudo -S apt update -y | pv -p -t -e -r -a -b > /dev/null
-echo $pass | sudo -S apt install python3 -y | pv -p -t -e -r -a -b > /dev/null 
 
 echo $pass | sudo -S chmod +x "$SCRIPT_DIR/run.sh"
 echo $pass | sudo -S chmod +x "$SCRIPT_DIR/config.sh"
