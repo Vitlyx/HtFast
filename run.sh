d9266a9a8160bdb169c4a8a0a29842b83d921665
Vit <103600474+Vitlyr@users.noreply.github.com>
2023-10-18 20:09:27 -0400

    `run` Code Debug

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # !! WARNING: DO NOT EDIT IN ANY WAY
 
-# Make sure docker is installed (below)
+# Make sure docker in installed (below)
 # Uninstall all unofficial docker files
 # for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 # Install docker
@@ -10,45 +10,50 @@
 
 # !! Make sure git is installed
 
-# Check if the config.sh file exists
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SCRIPT_DIR/config.sh
 
 git_repo=$1
-runCmd=$2
 
 eval "$pass"
 eval "$cloudflare_token"
-
-# Get the Cloudflare token ID
 cloudflare_token_id=$(echo "$cloudflare_token" | cut -d '-' -f 6)
 
-echo $pass | sudo -S echo ""
+# Assign the gnome-terminal to a different terminal
+export CLF_TERMINAL=xterm
+
+echo $pass | sudo -s echo ""
 if [[ $? -ne 0 ]]; then
   echo "Error: Incorrect password."
   exit 1
 fi
 
-# Function to terminate both terminal processes
 terminate() {
   echo "Ctrl+C pressed. Terminating the script and the terminal..."
-  pkill -P $$
+  kill "$terminal_pid"
+  kill "$uvicorn_pid"
   exit 1
 }
 trap terminate SIGINT
 
-gnome-terminal -- bash -c "echo $pass | sudo -S docker run cloudflare/cloudflared:latest tunnel --no-autoupdate run --token $cloudflare_token_id" &
+# Start the Cloudflare tunnel
+$CLF_TERMINAL -- bash -c "echo $pass | sudo -S docker run cloudflare/cloudflared:latest tunnel --no-autoupdate run --token $cloudflare_token_id" &
 terminal_pid=$!
 
-echo $git_repo
-
-repo_name=$(basename "$git_repo" .git)
-
+# Clone the Git repository
 git clone $git_repo
-cd $repo_name
 
+# Change to the cloned directory
+cd $(basename "$git_repo" .git)
+
+# Create a virtual environment and install the requirements
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt | pv -p -t -e -r -a -b > /dev/null
-uvicorn main:app --host 0.0.0.0 --port 8000
+
+# Start the Uvicorn server
+uvicorn main:app --host 0.0.0.0 --port 8000 &
+uvicorn_pid=$!
+
+# Wait for the user to press `Ctrl+C`
+wait "$terminal_pid" "$uvicorn_pid"
