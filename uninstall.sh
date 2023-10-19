61c42f40080081467f8b78cadd2a07beba53d915
Vit <103600474+Vitlyr@users.noreply.github.com>
2023-10-19 10:43:01 -0400

    `Uninstall` Wizard | 🛑

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # !! WARNING: DO NOT EDIT IN ANY WAY
-
-source config.sh
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPT_DIR/config.sh
 eval "$pass"
 echo "Are you sure you would like to uninstall this program? (y/n)"
 read -r un_agreement
@@ -10,5 +10,5 @@ if [[ "$un_agreement" != "y" ]]; then
   exit 1
 fi
 current_dir=$(pwd)
-echo $pass | sudo -s rm -r "$current_dir" -y | pv -p -t -e -r -a -b > /dev/null
+echo $pass | sudo -s rm -r "$current_dir"
 echo "Deleted successfully."
