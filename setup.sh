494b4797c5808efcb8b0cf2b4680576a074da96a
Vit <103600474+Vitlyr@users.noreply.github.com>
2023-10-19 12:04:46 -0400

    `Setup` Wizard | 🌟

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Prompt the user for input and store it in variables
 read -s -p "Enter your server's account password: " pass
 echo ""
 read -s -p "Enter your Cloudflare token (docker): " cloudflare_token
@@ -33,12 +32,10 @@ echo $pass | sudo -S apt install pv -y
 echo $pass | sudo -S apt install gnome-terminal -y | pv -p -t -e -r -a -b > /dev/null
 echo $pass | sudo -S apt update -y | pv -p -t -e -r -a -b > /dev/null
 
-# Change permissions of run.sh, config.sh, and uninstall.sh in the script's directory
 echo $pass | sudo -S chmod +x "$SCRIPT_DIR/run.sh"
 echo $pass | sudo -S chmod +x "$SCRIPT_DIR/config.sh"
 echo $pass | sudo -S chmod +x "$SCRIPT_DIR/uninstall.sh"
 
-# Use sudo to write to config.sh
 echo "pass=\"$pass\"" | sudo tee "$SCRIPT_DIR/config.sh" > /dev/null
 echo "cloudflare_token=\"$cloudflare_token\"" | sudo tee -a "$SCRIPT_DIR/config.sh" > /dev/null
 
